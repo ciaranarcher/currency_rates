12931c353d904daea1e71292706f5837960a7e5e
Ciaran Archer <ciaran.archer@gmail.com>
2012-08-25 11:11:45 +0100

    Added more rates

--- lib/currency_rates.rb
+++ lib/currency_rates.rb
@@ -6,7 +6,13 @@ module CurrencyRates
     get '/' do
       codes = [
         {:code => 'USD', :rate => 1.0},
-        {:code => 'EUR', :rate => 0.79521}
+        {:code => 'EUR', :rate => 0.79920},
+        {:code => 'INR', :rate => 55.4900},
+        {:code => 'AUD', :rate => 0.96066},
+        {:code => 'CAD', :rate => 0.99170},
+        {:code => 'ZAR', :rate => 8.38950},
+        {:code => 'NZD', :rate => 1.23267},
+        {:code => 'JPY', :rate => 78.6750}
       ]
 
       status 200 
